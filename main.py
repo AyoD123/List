3f8c2e26cae6e22561c13b704d7aad42c93168bf
AyoD123 <ayodejicoo@icloud.com>
2024-12-21 16:34:51 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+#Create a 2D - List
+matrix=[[1,2,3], [4,5,6], [7,8,9]]
+
+print(matrix)
+
+#Number of rows
+print(len(matrix))
+
+#Number of columns
+print(len(matrix[0]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
